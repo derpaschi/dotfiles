f05f45d1a47cace65666425ee7317ee6c7a3c131
Stefan Pasch <stefan.pasch@gmail.com>
2016-12-01 21:16:20 +0100

    Adjusted git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,6 +3,6 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
-alias gc='git commit'
+alias gc='git commit -S'
 alias gca='git commit -a'
 alias gco='git checkout'
