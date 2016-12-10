20c8fa721329be4027bb8217458934e33bd7414a
Stefan Pasch <stefan.pasch@gmail.com>
2016-12-10 14:08:30 +0100

    Updated key repeat reates.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -92,8 +92,8 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 13
 
 # Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
