ec8c55fe5481214f00de2607d18049865ae0207a
Stefan Pasch <stefan.pasch@gmail.com>
2017-10-06 14:50:34 +0200

    Correct compupstage command and let composer use the default php cli.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,5 +3,5 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias ssh='assh wrapper ssh' # assh wrapper
 alias ..='cd ..'
 alias wearerequiredup='cd ~/Development/wearerequired.local/_vagrant && vagrant up'
-alias compupstage='composer update && git add composer.json composer.lock && git commit -m "Composer update" && bundle exec cap staging deploy'
-alias composer='ssh -T git@github.com > /dev/null 2>&1 ; ssh -T git@bitbucket.org > /dev/null 2>&1 ; /usr/local/php5/bin/php /usr/local/bin/composer'
+alias compupstage='composer update && git add composer.json composer.lock && git commit -m "Composer update" && git push && bundle exec cap staging deploy'
+alias composer='ssh -T git@github.com > /dev/null 2>&1 ; ssh -T git@bitbucket.org > /dev/null 2>&1 ; /usr/local/bin/composer'
