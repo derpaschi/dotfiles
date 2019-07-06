e4aeeb6cde4d1f1f7fce5f9689fe4fae7e4018fd
Stefan Pasch <stefan.pasch@gmail.com>
2019-07-06 18:26:44 +0200

    Added newrelic ignore. Aliases added.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,11 @@ alias ..='cd ..'
 alias wearerequiredup='cd ~/Development/wearerequired.local/_vagrant && vagrant up'
 alias compupstage='composer update && git add composer.json composer.lock && git commit -m "Composer update" && git push && bundle exec cap staging deploy'
 alias composer='ssh -T git@github.com > /dev/null 2>&1 ; ssh -T git@bitbucket.org > /dev/null 2>&1 ; /usr/local/bin/composer'
+
+atlantis() {
+    pushd ~/.appflow/playbooks >/dev/null 2>&1
+    vagrant $@ atlantis
+    popd >/dev/null 2>&1
+}
+
+export COMPOSER_AUTH="`cat ~/.composer/auth.json`"
