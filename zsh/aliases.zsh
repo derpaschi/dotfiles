47b1c278fbffb28f555bee9e751c8d342b013287
Stefan Pasch <stefan.pasch@gmail.com>
2024-03-13 09:15:13 +0100

    Update l alias. Added server alias for composer server:...

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,9 +3,17 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias ssh="assh wrapper ssh --" # assh wrapper
 alias ..='cd ..'
 alias composer="ssh -T git@github.com > /dev/null 2>&1 ; ssh -T git@bitbucket.org > /dev/null 2>&1 ; `brew --prefix`/bin/composer"
-alias l='exa --long --header --git'
+alias l='ls -la'
 alias start='composer server:start'
 alias stop='composer server:stop'
+alias stopremove='composer server:stop --remove --skip-proxy'
+alias mysqladmin='/opt/homebrew/opt/mysql/bin/mysqladmin'
+
+# Alias `server <command>` to `composer server:<command>`.
+server() {
+	composer server:$@
+}
+
 #alias pygmy='pygmy-go'
 
 mockup() {
