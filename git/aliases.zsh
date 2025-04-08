9a854ae8df9cc8f3ef017e8fa6950a11c8a6f742
Stefan Pasch <stefan.pasch@gmail.com>
2025-04-08 12:27:31 +0200

    Updated git config

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,11 @@
+# Use `hub` as our git wrapper:
+#   https://hub.github.com/
+hub_path=$(which hub)
+if (( $+commands[hub] ))
+then
+  alias git=$hub_path
+fi
+
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
